ca36a09d84ae8ed4280e8ede09dca74b209fc5d4
amine8ghandi8amine <amine.ghandi.amine@gmail.com>
2018-01-30 16:14:20 +0000

    okokok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8">
+<meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
     <title>{{ config('app.name', 'Album') }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('css')
